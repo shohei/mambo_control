fcc5526326cf79b085bbd8cdbef2d439e7c138e4
Shohei Aoki <shoaok@gmail.com>
2024-01-15 18:48:29 +0300

    Add session state management for Mambo drone control

--- app.py
+++ app.py
@@ -1,69 +1,76 @@
 import streamlit as st
 from pyparrot.Minidrone import Mambo
+from session import _get_state
 
 st.title("Mambo control")
 
 mamboAddr = "d0:3a:aa:30:e6:5a"
 st.text("Mambo address: "+mamboAddr)
 
+state = _get_state()
+
+if state.mambo ==None:
+    state.mambo = 0
+
 i = st.empty()
-
-takeoff = st.button("Takeoff")
-
 duration=0.5
 
-mambo = Mambo(mamboAddr, use_wifi=False)
-i.write("trying to connect")
-success = mambo.connect(num_retries=3)
-i.write("connected: %s" % success)
-if (success):
-    # get the state information
-    i.write("sleeping")
-    mambo.smart_sleep(2)
-    mambo.ask_for_state_update()
-    mambo.smart_sleep(2)
+connect = st.button("Connect")
 
+if connect:
+    state.mambo = Mambo(mamboAddr, use_wifi=False)
+    i.write("trying to connect")
+    success = state.mambo.connect(num_retries=3)
+    i.write("connected: %s" % success)
+    if (success):
+        # get the state information
+        i.write("sleeping")
+        state.mambo.smart_sleep(2)
+        state.mambo.ask_for_state_update()
+        state.mambo.smart_sleep(2)
+
+takeoff = st.button("Takeoff")
 if takeoff:
     i.write("taking off!")
-    mambo.safe_takeoff(5)
+    state.mambo.safe_takeoff(5)
     
 forward = st.button("forward")
 if forward:
     i.write("Flying direct: going forward (positive pitch)")
-    mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=duration)
+    state.mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=duration)
 
 backward = st.button("backward")
 if backward:
     i.write("Flying direct: going backwards (negative pitch)")
-    mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=duration)
+    state.mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=duration)
 
 
 left = st.button("left")
 if left:
     i.write("Flying direct: roll left")
-    mambo.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=duration)
+    state.mambo.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=duration)
 
 right = st.button("right")
 if right:
     i.write("Flying direct: roll right")
-    mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=duration)
+    state.mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=duration)
 
 up = st.button("Up")
 if up:
     i.write("Flying direct: going up")
-    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=50, duration=1)
+    state.mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=50, duration=1)
 
 down = st.button("Down")
 if down:
     i.write("Flying direct: going down")
-    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-50, duration=1)
+    state.mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-50, duration=1)
 
 
 landing = st.button("land")
 if landing:
     i.write("landing")
-    mambo.safe_land(5)
-    mambo.smart_sleep(5)
+    state.mambo.safe_land(5)
+    state.mambo.smart_sleep(5)
 
     i.write("disconnect")
-    mambo.disconnect()
+    state.mambo.disconnect()
---
+++ session.py
@@ -0,0 +1,78 @@
+from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
+from streamlit.runtime import get_instance
+
+class _SessionState:
+    def __init__(self, session):
+        """Initialize SessionState instance."""
+        self.__dict__["_state"] = {
+            "data": {},
+            "hash": None,
+            "hasher": None,
+            "is_rerun": False,
+            "session": session,
+        }
+
+    def __call__(self, **kwargs):
+        """Initialize state data once."""
+        for item, value in kwargs.items():
+            if item not in self._state["data"]:
+                self._state["data"][item] = value
+
+    def __getitem__(self, item):
+        """Return a saved state value, None if item is undefined."""
+        return self._state["data"].get(item, None)
+        
+    def __getattr__(self, item):
+        """Return a saved state value, None if item is undefined."""
+        return self._state["data"].get(item, None)
+
+    def __setitem__(self, item, value):
+        """Set state value."""
+        self._state["data"][item] = value
+
+    def __setattr__(self, item, value):
+        """Set state value."""
+        self._state["data"][item] = value
+    
+    def clear(self):
+        """Clear session state and request a rerun."""
+        self._state["data"].clear()
+        self._state["session"].request_rerun()
+    
+    def sync(self):
+        """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
+
+        # Ensure to rerun only once to avoid infinite loops
+        # caused by a constantly changing state value at each run.
+        #
+        # Example: state.value += 1
+        if self._state["is_rerun"]:
+            self._state["is_rerun"] = False
+        
+        elif self._state["hash"] is not None:
+            if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
+                self._state["is_rerun"] = True
+                self._state["session"].request_rerun()
+
+        self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
+
+
+def _get_session():
+    runtime = get_instance()
+    session_id = get_script_run_ctx().session_id
+    session_info = runtime._session_mgr.get_session_info(session_id)
+
+    if session_info is None:
+        raise RuntimeError("Couldn't get your Streamlit Session object.")
+    
+    return session_info.session
+
+
+def _get_state():
+    session = _get_session()
+
+    if not hasattr(session, "_custom_session_state"):
+        session._custom_session_state = _SessionState(session)
+
+    return session._custom_session_state
+
